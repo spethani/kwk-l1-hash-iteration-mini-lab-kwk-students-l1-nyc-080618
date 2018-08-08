ad898c8bc7984470cff627afc541d14326cc2a69
spethani <sarahpethani@gmail.com>
2018-08-08 17:44:34 +0000

    automatically backed up by learn

--- summer_olympics_hash.rb
+++ summer_olympics_hash.rb
@@ -8,10 +8,12 @@ end
 def add_a_key_value_pair(summer_olympics)
   # Implement this method so that it adds a key value pair to the hash created in create_olympics_hash
   summer_olympics=create_olympics_hash()
+  puts summer_olympics
   summer_olympics[Atlanta]='1996'
   return summer_olympics
 end
 
+add_a_key_value_pair
 def iterate_through_hash
   # Implement this method so that it iterates over the hash created in add_a_key_value_pair
   # and puts each key value pair within the phrase "The _____ summer olympics took place in _____."
